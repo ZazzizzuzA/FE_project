e789161bee78cc4fd34e94cf1b0c5036e51b8dd0
Mykhailo Pylypenko <musicvyp@gmail.com>
2018-02-25 22:32:30 +0200

    Профайл есть, но можно еще украшать и украшать

--- src/modules/profile.vue
+++ src/modules/profile.vue
@@ -3,13 +3,16 @@
     div.block__profile#profile
         div.profile_photo
             img(src="http://images.entertainment.ie/images_content/rectangle/620x372/E-T.jpg" alt="photo")
-            
+            a.profile_link(type="button" href="https://github.com/ZazzizzuzA/FE_project" target="_blink") {{link.text}}        
         div.profile_info
             h3 {{name}} {{soname}}
             p.profile_location {{location}}
-            ul
-                li(v-for="fact in info") 
-                    input(v-model="fact.title")
+            p.profile_job {{job}}            
+            div.profile_questions
+                p.profile_facts(v-for="fact in info") {{fact.title}}
+            div.profile_answers
+                p.profile_facts(v-for="answer in answers" v-if="answer.show" @click="answer.show = !answer.show" ) {{answer.text}}
+                input(v-else="answer.show" @keyup.13="answer.show = !answer.show" v-model="answer.text")
 </template>
 
 <script>
@@ -20,19 +23,72 @@ export default {
             soname: "Pylypenko",
             location: "Ukraine, Kyiv",
             job: "Front-end Developer",
-            info: [ {
-                    title: "Age"
+            link: {
+                text: "This project on GitHub",
+            },
+            info: [ 
+                {
+                    title: "Nickname:",
                 },
                 {
-                    title: "Education"
-                }
+                    title: "Age:",
+                },
+                {
+                    title: "Birthday:",
+                },
+                {
+                    title: "Phone:",
+                },
+                {
+                    title: "Favorite movies:",
+                },
+                {
+                    title: "E-mail:",
+                },
+                {
+                    title: "Site:",
+                },
+
             ],
+            answers: [
+                {
+                    text: "Pomidorka",
+                    show: true
+                },
+                {
+                    text: "28",
+                    show: true                    
+                },
+                {
+                    text: "18.01.1990",
+                    show: true                    
+                },
+                {
+                    text: "+38-050-821-41-40",
+                    show: true                    
+                },
+                {
+                    text: "\"Brave heart\", \"Freinds\", \"Mr. Nobody\"",
+                    show: true                    
+                },
+                {
+                    text: "musicvyp@gmail.com",
+                    show: true                    
+                },
+                {
+                    text: "-",
+                    show: true                    
+                }
+            ]
+        }
+    },
+    methods: {
+        changeInfo: function(value) {
+            this.show = !this.show
         }
     }
 }
 	
-
-
 </script>
 
 <style lang="scss">
@@ -42,38 +98,94 @@ export default {
 .block__profile{
     display: flex;
     flex-flow: row nowrap;
-    width: 50%;
+    width: 70%;
     border-radius: 10px;
     margin: auto;
-    background-color: #c6fff0;
+    background-color: $bg_profile;
+    box-sizing: border-box;
     .profile_photo{
-        width: 50%;
         text-align: center;
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: space-around;
+        width: 40%;
+        align-items: center;
         img{
-            max-width: 75%;
+            width: 250px;
+            height: 200px;
             border-radius: 50%;
+        }
+        a{
+            @include padding(10px, 10px);
+            text-decoration: none;
+            border: 1px solid $green_bg;
+            width: 30%;
+            border-radius: 5px;
+            color: $indigo;
+            background-color: $blue;
         }
     }
     .profile_info{
         @include padding(10px, 10px);
         display: flex;
-        flex-flow: column nowrap;
+        flex-flow: row wrap;
         box-shadow: -5px 0px 4px $green_bg;
+        flex: 1 0 60%;    
+        box-sizing: border-box;
+            
         h3{
             @include margin(0px, 0px);
+            flex: 1 0 100%;
+            cursor: default;
             
         }        
-        p{
-            display: inline-block;
+        .profile_location{
             font-size: 0.6em;
             font-weight: 400;
-            margin: 0px 0px 10px 0px;
+            margin: 0px 0px 0px 0px;
+            flex: 1 0 100%;
+            cursor: default;
+            
         }
-        ul{
-            list-style-type: none;
+        .profile_job{
+            font-size: 0.9em;
+            margin: 0px 0px 10px 0px;
+            flex: 1 0 100%;
+            cursor: default;
+            
+        }
+        .profile_questions{
+            display: flex;
+            flex-flow: column wrap;
+            box-sizing: border-box;
+            width: 30%;
+            p {
+                margin: 0px 0px 20px 0px;
+                cursor: default;
+                text-shadow: 3px 2px 3px;
+            }
+        }
+        .profile_answers {
+            @include padding(0px, 10px);
+            display: flex;
+            flex-flow: column wrap;
+            box-sizing: border-box;
+            width: 70%;
+            border-left: 1px groove rgba(165, 214, 167, 0.356);
+            p {
+                margin: 0px 0px 20px 0px;
+                cursor: default;
+            }
+            p:hover{
+                cursor: pointer;
+                text-decoration: underline;
+                
+            }
+            input{
+                height: 18px;
+                margin-bottom: 20px;
+            }
         }
     }
 }
-
-
 </style>
